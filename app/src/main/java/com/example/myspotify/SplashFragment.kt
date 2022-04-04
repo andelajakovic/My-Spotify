8ea5ff037f07d3c6622c6f447544964a70fd4c50
Anđela Jaković <andela.jakovic@asseco-see.hr>
2022-04-04 23:32:49 +0200

    Welcome screen

--- app/src/main/java/com/example/myspotify/SplashFragment.kt
+++ app/src/main/java/com/example/myspotify/SplashFragment.kt
@@ -5,15 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.myspotify.databinding.FragmentSplashBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(1500)
+            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToWelcomeFragment())
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/myspotify/ui/prelogin/welcome/WelcomeFragment.kt
@@ -0,0 +1,39 @@
+package com.example.myspotify.ui.prelogin.welcome
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myspotify.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+    private fun initListeners() {
+        binding.loginLink.setOnClickListener {
+        }
+
+        binding.registerLink.setOnClickListener {
+        }
+    }
+}
